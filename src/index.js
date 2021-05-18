2e4106f64355f76f1e7791d9039c073647f606ce
mayralgr <ml17ft@gmail.com>
2021-05-17 22:18:16 -0500

    a

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 
 function Hi() {
-  return <p>Hi.</p>;
+  return <p>Hi. This is a react-redux app from 0 to 100</p>;
 }
 
 render(<Hi />, document.getElementById("app"));
