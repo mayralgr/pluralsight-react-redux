d4714382fd17204edc1c4cfe7841b3a06a96db40
mayralgr <ml17ft@gmail.com>
2021-05-25 22:04:10 -0500

    Bind functions

---
+++ src/components/CoursesPage.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+class CoursesPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      course: {
+        title: "",
+      },
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  }
+
+  render() {
+    return (
+      <form>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange} // this.handleChange.bind(this)
+          value={this.state.course.title}
+        />
+      </form>
+    );
+  }
+}
+
+export default CoursesPage;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
+import CoursesPage from "./components/CoursesPage";
 
-function Hi() {
-  return <p>Hi. This is a react-redux app from 0 to 100</p>;
-}
+// function Hi() {
+//   return <p>Hi. This is a react-redux app from 0 to 100</p>;
+// }
 
-render(<Hi />, document.getElementById("app"));
+// render(<Hi />, document.getElementById("app"));
+
+render(<CoursesPage />, document.getElementById("app"));
