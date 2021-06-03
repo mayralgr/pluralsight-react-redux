5ea21fe7b8b0aad29e30e25f048e8dd098021a55
mayralgr <ml17ft@gmail.com>
2021-06-02 22:49:16 -0500

    Add dispatch options

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
 class CoursesPage extends React.Component {
   state = {
     course: {
@@ -16,7 +17,8 @@ class CoursesPage extends React.Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    this.props.dispatch(courseActions.createCourse(this.state.course));
+    this.props.actions.createCourse(this.state.course);
+    //this.props.dispatch(courseActions.createCourse(this.state.course));
     //alert(this.state.course.title);
   };
 
@@ -41,7 +43,7 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
+  actions: PropTypes.object.isRequired,
   courses: PropTypes.array.isRequired,
 };
 
@@ -51,4 +53,11 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(CoursesPage);
+function mapDispatchToProps(dispatch) {
+  return {
+    //createCourse: (course) => dispatch(courseActions.createCourse(course)),
+    actions: bindActionCreators(courseActions, dispatch), // return them all wrapped in dispatch
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
